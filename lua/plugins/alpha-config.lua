f8b7eae0e72519bd42e284f5a5ff48f8c3f1f1ee
Jo <jo@pop-os.localdomain>
2022-08-11 17:58:59 -0400

    Fix git command.

--- lua/plugins/alpha-config.lua
+++ lua/plugins/alpha-config.lua
@@ -2,20 +2,20 @@ local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
 
 vim.cmd([[
-hi StartLogo1                  guifg=#14067E ctermfg=18   gui=NONE cterm=NONE
-hi StartLogo2                  guifg=#15127B ctermfg=18   gui=NONE cterm=NONE
-hi StartLogo3                  guifg=#171F78 ctermfg=18   gui=NONE cterm=NONE
-hi StartLogo4                  guifg=#182B75 ctermfg=18   gui=NONE cterm=NONE
-hi StartLogo5                  guifg=#193872 ctermfg=23   gui=NONE cterm=NONE
-hi StartLogo6                  guifg=#1A446E ctermfg=23   gui=NONE cterm=NONE
-hi StartLogo7                  guifg=#1C506B ctermfg=23   gui=NONE cterm=NONE
-hi StartLogo8                  guifg=#1D5D68 ctermfg=23   gui=NONE cterm=NONE
-hi StartLogo9                  guifg=#1E6965 ctermfg=23   gui=NONE cterm=NONE
-hi StartLogo10                 guifg=#1F7562 ctermfg=29   gui=NONE cterm=NONE
-hi StartLogo11                 guifg=#21825F ctermfg=29   gui=NONE cterm=NONE
-hi StartLogo12                 guifg=#228E5C ctermfg=29   gui=NONE cterm=NONE
-hi StartLogo13                 guifg=#239B59 ctermfg=29   gui=NONE cterm=NONE
-hi StartLogo14                 guifg=#24A755 ctermfg=35   gui=NONE cterm=NONE
+  hi StartLogo1                  guifg=#14067E ctermfg=18   gui=NONE cterm=NONE
+  hi StartLogo2                  guifg=#15127B ctermfg=18   gui=NONE cterm=NONE
+  hi StartLogo3                  guifg=#171F78 ctermfg=18   gui=NONE cterm=NONE
+  hi StartLogo4                  guifg=#182B75 ctermfg=18   gui=NONE cterm=NONE
+  hi StartLogo5                  guifg=#193872 ctermfg=23   gui=NONE cterm=NONE
+  hi StartLogo6                  guifg=#1A446E ctermfg=23   gui=NONE cterm=NONE
+  hi StartLogo7                  guifg=#1C506B ctermfg=23   gui=NONE cterm=NONE
+  hi StartLogo8                  guifg=#1D5D68 ctermfg=23   gui=NONE cterm=NONE
+  hi StartLogo9                  guifg=#1E6965 ctermfg=23   gui=NONE cterm=NONE
+  hi StartLogo10                 guifg=#1F7562 ctermfg=29   gui=NONE cterm=NONE
+  hi StartLogo11                 guifg=#21825F ctermfg=29   gui=NONE cterm=NONE
+  hi StartLogo12                 guifg=#228E5C ctermfg=29   gui=NONE cterm=NONE
+  hi StartLogo13                 guifg=#239B59 ctermfg=29   gui=NONE cterm=NONE
+  hi StartLogo14                 guifg=#24A755 ctermfg=35   gui=NONE cterm=NONE
 ]])
 
 -- Set header
@@ -63,7 +63,7 @@ local buttons = {
   dashboard.button("e", "  New file" , ":ene <BAR> startinsert <CR>"),
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("g", "  Git", ":Git <CR> | :wincmd j | :q<CR>"),
-  dashboard.button("s", "  Settings" , ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("s", "  Settings" , ":cd ~/.config/nvim | :e init.lua <CR>"),
   dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
