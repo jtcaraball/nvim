302060490805be40ffd62ed659297ddcfb220849
jtcaraball <jtcaraball@yahoo.com>
2022-08-25 10:25:33 -0400

    Fix vimtex pdf viewer option.

--- lua/plugins/vimtex-config.lua
+++ lua/plugins/vimtex-config.lua
@@ -2,5 +2,6 @@ vim.g.tex_flavor='latex'
 vim.g.vimtex_quickfix_mode=0
 
 if vim.loop.os_uname().sysname == "Darwin" then
-	vim.g.vimtex_view_general_viewr = 'skim'
+	print("Hello")
+	vim.g.vimtex_view_method = 'skim'
 end
