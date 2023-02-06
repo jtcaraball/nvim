77edd10d557394a2b81e620e19452f8b269c323f
Jo <jo@pop-os.localdomain>
2023-02-06 11:39:05 -0300

    Add live grep to menu.

--- lua/plugins/alpha-config.lua
+++ lua/plugins/alpha-config.lua
@@ -1,15 +1,17 @@
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
 
+nmap('<leader>a', ':Alpha <CR>')
+
 vim.cmd([[
-  hi StartLogo1 guifg=#143F6B 
-  hi StartLogo2 guifg=#344268 
-  hi StartLogo3 guifg=#544564 
-  hi StartLogo4 guifg=#744861 
-  hi StartLogo5 guifg=#954A5D 
-  hi StartLogo6 guifg=#B54D5A 
-  hi StartLogo7 guifg=#D55056 
-  hi StartLogo8 guifg=#F55353 
+  hi StartLogo1 guifg=#143F6B
+  hi StartLogo2 guifg=#344268
+  hi StartLogo3 guifg=#544564
+  hi StartLogo4 guifg=#744861
+  hi StartLogo5 guifg=#954A5D
+  hi StartLogo6 guifg=#B54D5A
+  hi StartLogo7 guifg=#D55056
+  hi StartLogo8 guifg=#F55353
 ]])
 
 -- Set header
@@ -56,23 +58,23 @@ end
 local buttons = {
   dashboard.button("e", "  New file" , ":ene <BAR> startinsert <CR>"),
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("l", "  Live grep", ":Telescope live_grep <CR>"),
   dashboard.button("g", "  Git", ":Git <CR> | :wincmd j | :q<CR>"),
   dashboard.button("s", "  Settings" , ":cd ~/.config/nvim | :e init.lua <CR>"),
-  dashboard.button("u", "痢 Update", ":PackerSync<CR>"),
+  -- dashboard.button("u", "痢 Update", ":PackerSync<CR>"),
   dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
 -- Send config to alpha
 local config = {
     layout = {
-        { type = "padding", val = 2 },
+        { type = "padding", val = 10 },
         colorize_header(),
-        { type = "padding", val = 2 },
-        { type = "padding", val = 2 },
+        { type = "padding", val = 6 },
         { type = "group", val = buttons, opts = {position = "center"} }
     },
     opts = {
         margin = 5,
     },
 }
-alpha.setup(config) 
+alpha.setup(config)
